29e3ae595fb0bdda1cc4a679e25477c41a77c222
Alexander Gadzhalov <alexander.gadzhalov@limechain.tech>
2024-11-07 20:29:01 +0200

    docs(GatekeeperTwo): style guide and comments

--- script/GatekeeperTwo.s.sol
+++ script/GatekeeperTwo.s.sol
@@ -14,24 +14,25 @@ contract Attacker {
     }
 }
 
+/// @author agadzhalov
+/// @title Solution to GatekeeperTwo Ethernaut challenge
+/// @notice Solution:
+///         Resources: https://consensys.github.io/smart-contract-best-practices/development-recommendations/solidity-specific/extcodesize-checks/
+///         BitwiseOps: https://www.youtube.com/watch?v=i2o4TfSC9nA
+///         XOR -> returns a 1 only if one of the bits equal to a 1. If both are 1 returns 0
+///         NOT -> Used to flip as type(uint64).max in binary are all 1s
+///         SHIFT RIGHT -> 63 needs to become 64
+///
+///         uint64(bytes8(keccak256(abi.encodePacked(address(this))))) = 1001001101110011011011111110100101000010001010001110111111010110
+///         uint64(_gateKey) = ?
+///         type(uint64).max = 1111111111111111111111111111111111111111111111111111111111111111
+///
+///         1. As we already know how the XOR works we need to flip uint64(bytes8(keccak256(abi.encodePacked(address(this)))))
+///         2. After flipping we can notice there's 1 bit difference.
+///            1001001101110011011011111110100101000010001010001110111111010110
+///            110110010001100100100000001011010111101110101110001000000101001
+///         3. Shift right
 contract GatekeeperTwoScript is Script {
-    /**
-     * Resources: https://consensys.github.io/smart-contract-best-practices/development-recommendations/solidity-specific/extcodesize-checks/
-     * BitwiseOps: https://www.youtube.com/watch?v=i2o4TfSC9nA
-     * XOR -> returns a 1 only if one of the bits equal to a 1. If both are 1 returns 0
-     * NOT -> Used to flip as type(uint64).max in binary are all 1s
-     * SHIFT RIGHT -> 63 needs to become 64 
-     * 
-     * uint64(bytes8(keccak256(abi.encodePacked(address(this))))) = 1001001101110011011011111110100101000010001010001110111111010110
-     * uint64(_gateKey) = ?
-     * type(uint64).max = 1111111111111111111111111111111111111111111111111111111111111111
-     * 
-     * 1. As we already know how the XOR works we need to flip uint64(bytes8(keccak256(abi.encodePacked(address(this)))))
-     * 2. After flipping we can notice there's 1 bit difference.
-     * 1001001101110011011011111110100101000010001010001110111111010110
-     * 110110010001100100100000001011010111101110101110001000000101001
-     * 3. Shift right 
-     */
     function run() public {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
         GatekeeperTwo gatekeeper = GatekeeperTwo(0x6c2F1ab4A6175E707964E7e681F298320FD33b43);
