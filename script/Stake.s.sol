1def2ffa944ac46b20ecac2ae0eb235bc4810641
Alexander Gadzhalov <alexander.gadzhalov@limechain.tech>
2024-11-12 21:18:21 +0200

    docs(Stake): style guide and comments

--- script/Stake.s.sol
+++ script/Stake.s.sol
@@ -26,30 +26,31 @@ contract Attacker {
     }
 }
 
+/// @author agadzhalov
+/// @title Solution to Stake Ethernaut challenge
+/// @notice Solution: 
+///          in the requirement there are four checks to be met in order to pass the challage.
+///          1. The Stake contract's ETH balance has to be greater than 0.
+///          2. totalStaked must be greater than the Stake contract's ETH balance.
+///          3. You must be a staker.
+///          4. Your staked balance must be 0.
+/// 
+///          1. StakeETH() is payable and we can execute it in order to send/stake ethers in the Stake contract
+///          2. We need to stake WETH as it's ERC20 and not ether. This way totalStake > contrat's ETH balace.
+///             But how? Usually WETH contract implements deposit() method, but in this case Stake.WETH hasn't impelemented it.
+///             How I know it? I've used online evm bytecode decoder looking at the ABI in order to figure out the methods implemented.
+///             https://app.dedaub.com/decompile.
+///          Hack: we can just approve as many tokens as we want. This is the tricky part of the challange
+///          Resource: to decode the selectors 0xdd62ed3e and 0x23b872dd you can use https://www.4byte.directory/
+///          3. Since we've already staked ETH and WETH we are already a staker.
+///          4. We execute Stake.Unstake(the total amout we staked (eth + weth))
+///          5. But this way 2. requirement is still not met.
+///             We just need to stake some more WETH from a different account.
 contract StakeScript is Script {
 
     Stake private stake = Stake(payable(0xA1ec9455be55D137Da1Dc08C8bBfCb01630c5901));
     IWETH private weth = IWETH(0xCd8AF4A0F29cF7966C051542905F66F5dca9052f);
 
-    /**
-     * Solution: in the requirement there are four checks to be met in order to pass the challage.
-     * 1. The Stake contract's ETH balance has to be greater than 0.
-     * 2. totalStaked must be greater than the Stake contract's ETH balance.
-     * 3. You must be a staker.
-     * 4. Your staked balance must be 0.
-     * 
-     * 1. StakeETH() is payable and we can execute it in order to send/stake ethers in the Stake contract
-     * 2. We need to stake WETH as it's ERC20 and not ether. This way totalStake > contrat's ETH balace.
-     *    But how? Usually WETH contract implements deposit() method, but in this case Stake.WETH hasn't impelemented it.
-     *    How I know it? I've used online evm bytecode decoder looking at the ABI in order to figure out the methods implemented.
-     *    https://app.dedaub.com/decompile.
-     *    Hack: we can just approve as many tokens as we want. This is the tricky part of the challange
-     *    Resource: to decode the selectors 0xdd62ed3e and 0x23b872dd you can use https://www.4byte.directory/
-     * 3. Since we've already staked ETH and WETH we are already a staker.
-     * 4. We execute Stake.Unstake(the total amout we staked (eth + weth))
-     * 5. But this way 2. requirement is still not met.
-     *    We just need to stake some more WETH from a different account.
-     */
     function run() public {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
 
